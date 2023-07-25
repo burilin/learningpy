ca59afb480039c46317b8f2dff9835e669ae62ed
collhoun <2904yr@mail.ru>
2023-07-25 14:42:02 +0300

    Select companies(modified)

--- servicepy/run.py
+++ servicepy/run.py
@@ -1,67 +1,63 @@
 import datetime
 import json
-
+# создать столько файлов в количестве отраслей. каждая отрасль в отдельный файл json
+# сделать commit
+#
+#
 
 def main():
     print('!run.py!')
-    cur = set()
-    with open("../data/data_stocks.json","r",encoding="utf-8") as file:
-        stocks = json.load(file)
-    for stock in range(len(stocks['instruments'])):
-        cur.add(stocks['instruments'][stock]['currency'])
-    #print(cur[0])
     
-    choose = input(f"выберите валюту для справки из данного списка:  {' ,'.join(list(cur))}\n")
-    if choose in cur:
-        for i in range(len(stocks['instruments'])):
-            
-            if stocks['instruments'][i]['currency'] == choose or stocks['instruments'][i]['nominal']['currency'] == choose:
-                print(f"{stocks['instruments'][i]}\n\n")
-            else:
-                print("такой валюты нет")
-                main()
-                break
 
        
-
-
-
 def fullinfo():
+    stroka = input("введи название интересующей компании ")
+    return stroka 
+
+
+def selsect_stocks():
     # в переменной companies храним название компаний
-    companies = ""
-    with open("../data/data_stocks.json","r",encoding="utf-8") as file:
-       stocks = json.load(file)
-    for i in range(len(stocks['instruments'])):
-       companies += f"{stocks['instruments'][i]['name']}"
+    companies = []
+    stroka = fullinfo()
+    stocks = writer()
     
-    company = input("введи интересующую компанию ")
-    res = ""
-    if company in companies:
-        for i in range(len(stocks['instruments'])):
-            if company == f"{stocks['instruments'][i]['name']}":
-                res = f"{stocks['instruments'][i]}"
-                break
-        print(res)
-    else:
-        fullinfo()
+    for i in range(len(stocks['instruments'])):
+       companies.append( f"{stocks['instruments'][i]['name']}")
+       
+    
+    
+    res = []
+
+    for i in range(len(stocks['instruments'])):
+        if stroka in f"{stocks['instruments'][i]['name']}":
+            res.append(f"{stocks['instruments'][i]}")
+        
+            
+            
+    return res 
 
 
 
 
-def helper():
+def writer():
    
     with open("../data/data_stocks.json","r",encoding="utf-8") as file:
        stocks = json.load(file)
-    for i in range(16):
-        print(f"{stocks['instruments'][i]}\n\n")
+    return stocks 
 
-       
+
+def output():
+    out = selsect_stocks()
+    if out:
+        for i in out:
+            print(f"{i}\n\n")
+    else:
+        print("такой компании нет")
+
    
 
 
 if __name__ == '__main__':
-    #main()
-    fullinfo()
-    #helper()
+    output()
     
 
