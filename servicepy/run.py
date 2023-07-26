aac45f831cf17b58bd83e9392f45785b3ccb0dc3
collhoun <2904yr@mail.ru>
2023-07-26 14:54:40 +0300

    Only sector in each json

--- servicepy/run.py
+++ servicepy/run.py
@@ -9,12 +9,12 @@ def main():
     print('!run.py!')
     
 
-       
+# ввод имени компании
 def fullinfo():
     stroka = input("введи название интересующей компании ")
     return stroka 
 
-
+# поиск компаний по заданной строке
 def selsect_stocks():
     # в переменной companies храним название компаний
     companies = []
@@ -23,29 +23,22 @@ def selsect_stocks():
     
     for i in range(len(stocks['instruments'])):
        companies.append( f"{stocks['instruments'][i]['name']}")
-       
-    
-    
+
     res = []
 
     for i in range(len(stocks['instruments'])):
         if stroka in f"{stocks['instruments'][i]['name']}":
-            res.append(f"{stocks['instruments'][i]}")
-        
-            
-            
+            res.append(f"{stocks['instruments'][i]}")        
     return res 
 
-
-
-
+# открытие json файла
 def writer():
    
     with open("../data/data_stocks.json","r",encoding="utf-8") as file:
        stocks = json.load(file)
     return stocks 
 
-
+# вывод 
 def output():
     out = selsect_stocks()
     if out:
@@ -56,8 +49,35 @@ def output():
 
    
 
+def creating_json():
+    stocks = writer()
+
+    sectors = set()
+
+    for i in range(len(stocks['instruments'])):
+        sectors.add(stocks['instruments'][i]["sector"])
+    
+    for i in sectors:
+    
+        data =[]
+        for j in range(len(stocks['instruments'])):
+            if i == stocks['instruments'][j]["sector"]:
+                data.append(stocks['instruments'][j])
+            
+
+        with open(i+'.json','w') as file:
+            main_data ={'main': data}
+            json.dump(main_data, file, indent= 4)
+    
+    
+
+
+
+
+
+
 
 if __name__ == '__main__':
-    output()
-    
+    #output()
+    creating_json()
 
