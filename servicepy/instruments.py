4dda25f8544113ad764fb9a90555a09eaf239a79
collhoun <2904yr@mail.ru>
2023-09-08 22:20:05 +0300

    Updating menu and adding params in funcs

--- servicepy/instruments.py
+++ servicepy/instruments.py
@@ -1,58 +1,64 @@
 import json
 import os
 import collections
-#создать экземпляр класса, чтобы проверить creating json
-class Stocks:
+# создать экземпляр класса, чтобы проверить creating json, изменив структуру создания json+
+# переименовать класс Stocks в Instruments+ 
+# переименовать writer в reader +
+# file_path_out/ file_path_in-
+# передать как параметры поиска search_key, value+
 
-    def __init__(self, file_path = "../data/data_stocks.json" , search_key = "ticker", search_value = "SBERP"):
+class Instruments:
+
+    def __init__(self, file_path = "../data/data_stocks.json" ):
         self.file_path = file_path
-        self.search_key = search_key
-        self.search_value = search_value
-        self.stocks = self.writer()
+        self.stocks = self.reader()
         self.len_stocks = len(self.stocks)
 
     def __repr__(self) -> str:
         return str("self.stocks_list_dict")
     
 
-    def writer(self) -> list:   # opening file
+    def reader(self) -> list:   # opening file
 
         with open(self.file_path,"r",encoding="utf-8") as file:
             stocks = json.load(file)
         return stocks['instruments'] 
     
     
-    def select_stocks(self, string:str ) -> list: # slow select stocks by string(name)
-        
-        companies = []
-        
-        for i in range(self.len_stocks):
-            companies.append( f"{self.stocks[i]['name']}")
+    def select_stocks(self, search_key:str ,search_value:str ) -> list: # slow select stocks by string(name)
+        try:
+    
+            res = []
 
-        res = []
+            for i in range(self.len_stocks):
 
-        for i in range(self.len_stocks):
-
-            if string.lower() in f"{self.stocks[i]['name']}":
-                res.append(f"{self.stocks[i]}")        
-        return res 
+                if search_value.lower() in f"{self.stocks[i][search_key].lower()}":
+                    res.append(f"{self.stocks[i]}")        
+            return res 
+        except Exception as ex:
+            return f'Error. {ex}.'
     
 
 
-    def sort_list_dict(self, stocks:list) -> list:  # quick sorting
+    def sort_list_dict(self, stocks:list, search_key:str ) -> list:  # quick sorting
         if len(stocks) <=1:
             return stocks
         base_elem = stocks[0]
 
-        left = [elem for elem in stocks if elem[self.search_key] < base_elem[self.search_key]]
-        center = [i for i in stocks if i[self.search_key] == base_elem[self.search_key]]
-        right = [elem for elem in stocks if elem[self.search_key] > base_elem[self.search_key]]
-        return self.sort_list_dict(left) + center + self.sort_list_dict(right)
+        left = [elem for elem in stocks if elem[search_key] < base_elem[search_key]]  
+        center = [i for i in stocks if i[search_key] == base_elem[search_key]]
+        right = [elem for elem in stocks if elem[search_key] > base_elem[search_key]]
+        return self.sort_list_dict(left, search_key) + center + self.sort_list_dict(right, search_key)
+    
+
+    def sort_list_dict_public(self, search_key):
+        copy_stocks = self.stocks
+        result = self.sort_list_dict(copy_stocks, search_key)
+        return result
 
 
-
-    def select_stocks2(self) -> list: # binary search by the key and its value
-        list_dict = self.sort_list_dict()
+    def select_stocks2(self, search_key:str, search_value:str) -> list: # binary search by the key and its value
+        list_dict = self.sort_list_dict_public(search_key)
 
 
 
@@ -61,23 +67,24 @@ class Stocks:
         while left<=right:
             mid = (left+right)//2
 
-            if list_dict[mid][self.search_key] == self.search_value:
+            if list_dict[mid][search_key] == search_value:
                 return list_dict[mid]
-            if list_dict[mid][self.search_key] > self.search_value:
+            if list_dict[mid][search_key] > search_value:
                 right = mid - 1
-            if list_dict[mid][self.search_key] < self.search_value:
+            if list_dict[mid][search_key] < search_value:
                 left = mid + 1
         return "not found"
     
-    def creating_json(self, search_par:str) -> str:    #creating json using searching par
+    
+    def writer(self, search_par:str) -> str:    #creating json using searching par
 
-        par = set()
+        params = set()
         
         try:
             for i in range(self.len_stocks):
-                par.add(self.stocks[i][search_par])
+                params.add(self.stocks[i][search_par])
             os.makedirs(f'../data/{str(search_par)}')
-            for i in par:
+            for i in params:
             
                 data =[]
                 for j in range(self.len_stocks):
@@ -86,11 +93,11 @@ class Stocks:
                     
                 
                 with open(f'../data/{str(search_par)}/'+str(i)+'.json','w',encoding='utf-8') as file:
-                    main_data ={'main': data}
+                    main_data ={'instruments': data}
                     json.dump(main_data, file, indent= 4)
             return "success"
         except Exception:
-            return "it's impossible to sort by this tag"
+            return "it's impossible to write and sort by this tag"
         
         
     def country_popularity_rating(self) -> collections:  # rate stocks by popularity
@@ -99,13 +106,13 @@ class Stocks:
         return result
     
 
-    def companies_by_date(self, year:int) -> list: # searching companies by year
+    def companies_by_date(self, year:str) -> list: # searching companies by year
         companies_list = []
 
 
         for i in range(self.len_stocks):
             keys = self.stocks[i].keys()
-            if 'first1dayCandleDate' in keys and str(year) in self.stocks[i]['first1dayCandleDate']:
+            if 'first1dayCandleDate' in keys and year in self.stocks[i]['first1dayCandleDate']:
                 companies_list.append(self.stocks[i])
         if companies_list:
             return companies_list
--- servicepy/run.py
+++ servicepy/run.py
@@ -1,4 +1,4 @@
-import stocks
+import instruments
 ### Tasks ###
 # Cоздать столько файлов в количестве отраслей. каждая отрасль в отдельный файл json+
 # Cделать commit опубликовать свои изменения на  GitHub+-
@@ -18,28 +18,39 @@ import stocks
 
 
 #---------------------------------------------------------------------------------------------------------------------------
-instrument = stocks.Stocks()
+instrument = instruments.Instruments()
+instrument2 = instruments.Instruments(file_path="../data/sector/consumer.json")
 
 
-def main(*args) -> None:
+def main() -> None:
     
     tasks = {
-        1: {"name_task_rus" : "Поиск акций по наименованию(частичное вхождение)", "sys_name_function" : instrument.select_stocks, "funcs_params":{"par_name":"string", "par_type":"str"}},
-        2: {"name_task_rus" : "Создание папки с файлами по ключу", "sys_name_function" : instrument.creating_json,  "funcs_params":{"par_name":"search_par", "par_type":"str"}},
+        1: {"name_task_rus" : "Поиск акций по наименованию(частичное вхождение)", "sys_name_function" : instrument.select_stocks, "funcs_params":{"params_names":["search_value", "search_key"], "params_type":"str"}},
+        2: {"name_task_rus" : "Создание папки с файлами по ключу", "sys_name_function" : instrument.writer,  "funcs_params":{"par_name":"search_par", "par_type":"str"}},
         3: {"name_task_rus" : "Рейтинг по частоте появления акций различных стран" ,"sys_name_function" : instrument.country_popularity_rating,  "funcs_params":"No need in params"},
         4: {"name_task_rus" : "Поиск компаний по заданному году пояления акций на бирже" ,"sys_name_function" : instrument.companies_by_date,  "funcs_params":{"par_name":"year", "par_type":"int"}},
         5: {"name_task_rus" : "Поиск компании, чьи акции появились появились раньше других" ,"sys_name_function" : instrument.the_oldest_company,"funcs_params":"No need in params"},
-        6: {"name_task_rus" : "Поиск акций по наименованию(полное вхождение)" ,"sys_name_function" : instrument.select_stocks2,"funcs_params":"No need in params"}  # ??????
+        6: {"name_task_rus" : "Поиск акций по наименованию(полное вхождение)" ,"sys_name_function" : instrument.select_stocks2,"funcs_params":{"params_names":["search_value", "search_key"], "params_type":"str"}}
     }
-    print("выбери функцию")
+    lis_no_params = [i for i in range(1,len(tasks)+1) if tasks[i]['funcs_params']=="No need in params"]
+    lis_one_param = [i for i in range(1,len(tasks)+1) if "par_name" in tasks[i]['funcs_params']]
+    lis_some_params = [i for i in range(1,len(tasks)+1) if "params_names" in tasks[i]['funcs_params']]
+
+
+    print("Привет, выбери функцию для работы с json-обьектом (введи номер из списка ниже)")
     [print(str(i) +' ------- ' + tasks[i]['name_task_rus']) for i in range(1,len(tasks)+1)]
-    task = int(input())
-
-    if not (task == 3 or task == 5 or task == 6):
-        param = input("введи значение параметра ")
+    task = int(input())  #сделать красивый и понятный input
+    if task in lis_no_params:
+        return tasks[task]['sys_name_function']()
+    elif task in lis_one_param:
+        param = input(f"Вы выбрали функцию, для выполнения которой нужен параметр (введи его): {tasks[task]['funcs_params']}: ")
         return tasks[task]['sys_name_function'](param)
-
-    return tasks[task]['sys_name_function']()
+    elif task in lis_some_params:
+        params = input(f"""Вы выбрали функцию, для выполнения которой нужны параметры: {tasks[task]['funcs_params']}
+Введите параметры через запятую без пробела: """).split(',')
+        return tasks[task]['sys_name_function'](*params)
+    else:
+        return "функции под таким номером не существует"
 
 
 
