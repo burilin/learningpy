16dcf9dea059679f8259f826bf496acfaf40c11c
collhoun <2904yr@mail.ru>
2023-09-08 13:18:08 +0300

    Runnig run.py importing methods from stocks.py

--- servicepy/run.py
+++ servicepy/run.py
@@ -24,24 +24,28 @@ instrument = stocks.Stocks()
 def main(*args) -> None:
     
     tasks = {
-        1: {"name_task_rus" : "Поиск акций по наименованию(частичное вхождение)", "sys_name_function" : instrument.select_stocks},
-        2: {"name_task_rus" : "Создание папки с файлами по ключу", "sys_name_function" : instrument.creating_json},
-        3: {"name_task_rus" : "Рэйтинг по частоте появления акций различных стран" ,"sys_name_function" : instrument.country_popularity_rating},
-        4: {"name_task_rus" : "Поиск компаний по заданному году пояления акций на бирже" ,"sys_name_function" : instrument.companies_by_date},
-        5: {"name_task_rus" : "Поиск компании, чьи акции появились появились раньше других" ,"sys_name_function" : instrument.the_oldest_company},
-        6: {"name_task_rus" : "Поиск акций по наименованию(полное вхождение)" ,"sys_name_function" : instrument.select_stocks2}
+        1: {"name_task_rus" : "Поиск акций по наименованию(частичное вхождение)", "sys_name_function" : instrument.select_stocks, "funcs_params":{"par_name":"string", "par_type":"str"}},
+        2: {"name_task_rus" : "Создание папки с файлами по ключу", "sys_name_function" : instrument.creating_json,  "funcs_params":{"par_name":"search_par", "par_type":"str"}},
+        3: {"name_task_rus" : "Рейтинг по частоте появления акций различных стран" ,"sys_name_function" : instrument.country_popularity_rating,  "funcs_params":"No need in params"},
+        4: {"name_task_rus" : "Поиск компаний по заданному году пояления акций на бирже" ,"sys_name_function" : instrument.companies_by_date,  "funcs_params":{"par_name":"year", "par_type":"int"}},
+        5: {"name_task_rus" : "Поиск компании, чьи акции появились появились раньше других" ,"sys_name_function" : instrument.the_oldest_company,"funcs_params":"No need in params"},
+        6: {"name_task_rus" : "Поиск акций по наименованию(полное вхождение)" ,"sys_name_function" : instrument.select_stocks2,"funcs_params":"No need in params"}  # ??????
     }
+    print("выбери функцию")
+    [print(str(i) +' ------- ' + tasks[i]['name_task_rus']) for i in range(1,len(tasks)+1)]
+    task = int(input())
 
+    if not (task == 3 or task == 5 or task == 6):
+        param = input("введи значение параметра ")
+        return tasks[task]['sys_name_function'](param)
 
-    task = int(input(f"выбери функцию {tasks} "))
-
-    return tasks[task]['sys_name_function'](*args)
+    return tasks[task]['sys_name_function']()
 
 
 
 
 if __name__ == '__main__':
-    result = main(instrument.stocks_list_dict)
+    result = main()
     print(result)
     
 
