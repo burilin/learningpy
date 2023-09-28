0be1b99d0afba781d1a44a56e96d5c4cb43f6d6d
collhoun <2904yr@mail.ru>
2023-09-28 20:50:02 +0300

    Adding modified tinkoff_data

--- servicepy/tinkoff_data.py
+++ servicepy/tinkoff_data.py
@@ -1,50 +1,48 @@
 import json
 import requests
 import os
-
-class Tinkoff_Data:
+# жетско зашить токен без передачи self.token_secret либо вынести в конфиг файл+
+# убрать stocks_path, добавить массив figi, период (from/ to) в иннит+
+# создать loader, путь сохранения+-
+# назвать папку файлов свечи +
+# вынести в меню+
+class TinkoffData:
 
     def __init__(self,
                 url_post = "https://sandbox-invest-public-api.tinkoff.ru/rest/tinkoff.public.invest.api.contract.v1.MarketDataService/GetCandles",
-                token = "t.ZDlrOhEC9ykat6XRj1laFX4NLWoI6WgOYp-thfy7ceafgAt16S4hqMqQisDNp1sUceg1-2diDXa2J5dox9i_3Q",
-                stocks_path = "../data/data_stocks.json"):
+                figi = ["BBG004730N88"],
+                date_from = "2022-06-19T14:41:49.263Z",
+                date_to = "2022-09-19T14:41:49.263Z"):
+        self.figies = figi
+        self.date_from = date_from
+        self.date_to = date_to
         self.url_post = url_post
-        self.token = token
-        self.stocks_path = stocks_path
-        self.stocks = self.reader() # file data_stocks.json
-        self.headers = {"Authorization": f"Bearer {token}",
+        self.token = "t.ZDlrOhEC9ykat6XRj1laFX4NLWoI6WgOYp-thfy7ceafgAt16S4hqMqQisDNp1sUceg1-2diDXa2J5dox9i_3Q"
+        self.headers = {"Authorization": f"Bearer {self.token}",
                                 "accept": "application/json",
                                 "Content-Type": "application/json"}
-    def get_figies_private(self) -> object:   #getting figies
-        return [i["figi"] for i in self.stocks]
+
         
-    def set_data_json(self,figi:str)->dict:     #setting data dictionary, depending on figi value
+    def create_data_json_private(self,figi:str, date_from:str, date_to:str)->dict:     #setting data dictionary, depending on figi value
         return {
-        "figi": figi, "from": "2022-06-19T14:41:49.263Z",
-        "to": "2022-09-19T14:41:49.263Z", "interval": "CANDLE_INTERVAL_DAY"}
+        "figi": figi, "from": date_from,
+        "to": date_to, "interval": "CANDLE_INTERVAL_DAY"}
 
     def get_data_json(self, data_json: dict)->json:     #requesting to url, getting json using api 
         return requests.post(self.url_post, headers=self.headers, data=json.dumps(data_json)).json()
     
-    def reader(self) -> list:   # opening file
 
-        with open(self.stocks_path,"r",encoding="utf-8") as file:
-            stocks = json.load(file)
-        return stocks['instruments'] 
 
-    def writer(self) -> str:
-        try:
-            os.makedirs('../data/figies_datas')
-            for figi in self.get_figies_private():
-                with open(f'../data/figies_datas/{figi}.json', "w", encoding='utf-8'):
-                    self.get_data_json(self.set_data_json(figi))
-        except Exception:
-            return Exception
+    def loader(self) -> str:
+        if not os.path.isdir('../data/candles'):
+            os.makedirs('../data/candles')
+        for figi in self.figies:
+            with open(f'../data/candles/{figi}.json', "w", encoding='utf-8') as file:
+                json.dump(self.get_data_json(self.create_data_json_private(figi, self.date_from, self.date_to)),file,indent=4)
+
         
         return "success"
 
     def __repr__(self) -> str:
         return "class Tinkoff_Data"
     
-tin = Tinkoff_Data()
-print(tin.writer())
