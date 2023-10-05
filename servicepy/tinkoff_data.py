ad5f4c4a06bd164fbc48942d56acfe13bc19b1fc
collhoun <2904yr@mail.ru>
2023-10-05 21:03:46 +0300

    Separate date

--- servicepy/tinkoff_data.py
+++ servicepy/tinkoff_data.py
@@ -10,6 +10,11 @@ import datetime
 # ввод данных от пользователя+
 # если дата больше двух лет, то разбиваем на несколько запросов+
 # stock.py посмотреть задания+
+# удалить костыли мерки года
+#
+#
+#
+ONE_YEAR = datetime.timedelta(365)
 class TinkoffData:
 
     def __init__(self,
@@ -29,41 +34,37 @@ class TinkoffData:
     def get_data_json(self, data_json: dict)->json:     #requesting to url, getting json using api 
         response = requests.post(self.url_post, headers=self.headers, data=json.dumps(data_json)).json()
         if response['candles']:
-            return response
+            return response['candles']
         raise KeyError
 
-    def check_date_private(self, date_from:str, date_to:str):
-        const_compare_date = datetime.datetime.strptime("2022-09-19T14:41:49.263Z", "%Y-%m-%dT%H:%M:%S.%fZ") - datetime.datetime.strptime("2021-09-19T14:41:49.263Z", "%Y-%m-%dT%H:%M:%S.%fZ")
+    def check_date_private(self, date_from:str, date_to:str) -> bool:
         try:
             parsed_date_from = datetime.datetime.strptime(date_from, "%Y-%m-%dT%H:%M:%S.%fZ")
             parsed_date_to = datetime.datetime.strptime(date_to, "%Y-%m-%dT%H:%M:%S.%fZ")
-            if parsed_date_to - parsed_date_from <= const_compare_date:
+            if parsed_date_to - parsed_date_from <= ONE_YEAR:
                 return True
             else:
                 return False
         except ValueError:
             raise ValueError 
 
-    def separate_date_private(self,figi,  date_from, date_to):
+    def separate_date_private(self,figi:str, date_from:str, date_to:str):
         dates = []
-
-        const_compare_date = datetime.datetime.strptime("2022-09-19T14:41:49.263Z", "%Y-%m-%dT%H:%M:%S.%fZ") - datetime.datetime.strptime("2021-09-19T14:41:49.263Z", "%Y-%m-%dT%H:%M:%S.%fZ")
 
         parsed_date_from = datetime.datetime.strptime(date_from, "%Y-%m-%dT%H:%M:%S.%fZ")
         parsed_date_to = datetime.datetime.strptime(date_to, "%Y-%m-%dT%H:%M:%S.%fZ")
         while parsed_date_to > parsed_date_from:
             dates.append(parsed_date_from)
-            parsed_date_from += const_compare_date
+            parsed_date_from += ONE_YEAR
             
         dates.append(parsed_date_to)
-        dates = [ str(datetime.datetime.strftime(i, "%Y-%m-%dT%H:%M:%S.%fZ")) for i in dates]
-        print(dates)
+        dates = [str(datetime.datetime.strftime(i, "%Y-%m-%dT%H:%M:%S.%fZ")) for i in dates]
+
         data = []
         for i in range(1,len(dates)):
-            data.extend(self.get_data_json(self.create_data_json_private(figi, dates[i-1], dates[i]))['candles'])
-        print(data)
+            data.extend(self.get_data_json(self.create_data_json_private(figi, dates[i-1], dates[i])))
+        return data
 
-        # здесь должно быть продолжение функции
 
     def loader(self, figi:str = "BBG004730N88", date_from:str = "2022-06-19T14:41:49.263Z", date_to:str = "2022-09-19T14:41:49.263Z") -> bool:
         try:
@@ -81,10 +82,20 @@ class TinkoffData:
                 
                 return True
             else: 
-                self.separate_date_private(figi, date_from,date_to)
+                data = {"candles":self.separate_date_private(figi, date_from,date_to)}
+                try:
+                    
+                    if not os.path.isdir('../data/candles'):
+                        os.makedirs('../data/candles')
+                    
+                    with open(f'../data/candles/{figi}.json', "w", encoding='utf-8') as file:
+                        json.dump(data,file,indent=4)
+                    return True
+                except KeyError():
+                    return False
         except ValueError:
             return False
-        
+   
 
     def __repr__(self) -> str:
         return "class Tinkoff_Data"
@@ -92,11 +103,4 @@ class TinkoffData:
 if __name__ == "__main__":
     tin = TinkoffData()
     tin_func = tin.loader
-    assert tin_func("BBG004730N88",  "2021-09-19T14:41:49.263Z","2022-09-19T14:41:49.263Z") == True
-    assert tin_func("BBG004730N88",  "2021-09-19T14:41:49.263","2022-09-19T14:41:49.263Z")== False
-    assert tin_func("f",  "2021-09-19T14:41:49.263Z","2022-09-19T14:41:49.263Z")== False
-    assert tin_func("BBG004730N88",  "f","2022-09-19T14:41:49.263Z")== False
-    assert tin_func("BBG004730N88",  "2021-09-19T14:41:49.263Z","f")== False
-    assert tin_func("BBG004730N88",  "2021-09-19","2022-09-19T14:41:49.263Z")== False
-    assert tin_func("BBG004730N88",  "2021-09-19T14:41:49.263Z","2022-09")== False
-    tin.separate_date_private("BBG004730N88","2020-06-19T14:41:49.263Z","2022-09-19T14:41:49.263Z")
+    print(tin.separate_date_private("BBG004730N88","2020-06-19T14:41:49.263Z","2022-09-19T14:41:49.263Z"))
