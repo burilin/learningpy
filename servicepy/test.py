f2a783a9b24381a79bdaeebe19e897be7fae2223
collhoun <2904yr@mail.ru>
2023-11-18 19:59:08 +0300

    Two lines on graphic

--- servicepy/test.py
+++ servicepy/test.py
@@ -13,9 +13,9 @@ from flask import Flask, render_template, url_for, request, redirect, g
 # tinkoff = tinkoff_data.TinkoffData()
 # signal = signals.Signals()
 # s = {'figi': 'TCS007661625', 'ticker': 'GAZP', 'classCode': 'SMAL', 'isin': 'RU0007661625', 'lot': 1, 'currency': 'rub', 'shortEnabledFlag': False, 'name': 'Газпром', 'exchange': 'MOEX_PLUS', 'ipoDate': '1998-12-30T00:00:00Z', 'issueSize': '23673512900', 'countryOfRisk': 'RU', 'countryOfRiskName': 'Российская Федерация', 'sector': 'energy', 'issueSizePlan': '23673512900', 'nominal': {'currency': 'rub', 'units': '5', 'nano': 0}, 'tradingStatus': 'SECURITY_TRADING_STATUS_NORMAL_TRADING', 'otcFlag': False, 'buyAvailableFlag': True, 'sellAvailableFlag': True, 'divYieldFlag': True, 'shareType': 'SHARE_TYPE_COMMON', 'minPriceIncrement': {'units': '0', 'nano': 10000000}, 'apiTradeAvailableFlag': False, 'uid': '9100df58-0e92-48ec-b962-4e788e6b61fd', 'realExchange': 'REAL_EXCHANGE_MOEX', 'positionUid': '4f9d0c81-cdf9-4735-8295-bacbfa3b8a51', 'forIisFlag': True, 'forQualInvestorFlag': False, 'weekendFlag': True, 'blockedTcaFlag': False, 'liquidityFlag': True, 'first1minCandleDate': '2018-03-08T04:23:00Z', 'first1dayCandleDate': '2018-03-13T07:00:00Z'}
+import json
+import ast
 
-import matplotlib
-import matplotlib.pyplot as plt
-print(matplotlib.get_backend())
-plt.plot([(0,0), (1,1), (2,4), (3,9)])
-plt.show()
+list = "['search_key', 'search_value']"
+list = ast.literal_eval(list)
+print(list[1])
